2de527ab8f0e4f0b7864aad76e549aee67251601
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-04-27 12:02:08 -0600

    finish Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;
+    int sum = 1 + 2 +3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20;
+
+    do {
+      System.out.println(sum);
+      count = count + sum;
+    } while(count <= 20);
+  }
+}
