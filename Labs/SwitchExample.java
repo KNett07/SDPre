264408f2b7911138e95f7ce88c8b8142b6f4dd70
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-04-27 10:10:43 -0600

    finish SwitchExample

---
+++ Labs/SwitchExample.java
@@ -0,0 +1,33 @@
+public class SwitchExample {
+  public static void main(String[] args) {
+    char grade;
+    grade = 'c';
+
+    switch (grade) {
+      case 'A':
+      case 'a':
+      System.out.println("Fantastic job!");
+      break;
+      case 'B':
+      case 'b':
+      System.out.println("Good job!");
+      break;
+      case 'C':
+      case 'c':
+      System.out.println("You're doing okay.");
+      break;
+      case 'D':
+      case 'd':
+      System.out.println("Please schedule a conference.");
+      break;
+      case 'F':
+      case 'f':
+      System.out.println("Start finishing your homework and coming to class.");
+      break;
+      default:
+      System.out.println("Invalid grade.");
+
+
+        }
+  }
+}
