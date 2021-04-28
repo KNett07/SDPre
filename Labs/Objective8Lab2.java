3d25df445419d4bc4f16cb9ac1afc7100eb159a2
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-04-28 12:25:10 -0600

    finish Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,15 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    int end;
+
+    for (int i = 1; i < 21; i++){
+    int sum = (i + 1) + i;
+    end = 20;
+  System.out.println(sum);
+}
+
+
+  }
+}
