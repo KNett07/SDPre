16c49367ca6f2830aeb2a343ef82edd8b9574f6a
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-04-28 11:34:02 -0600

    finish Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,19 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    int end = 10;
+    int total = 10;
+    for (int i = 1; i < 11; i++){
+       total += i;
+    System.out.println(i);
+  }
+
+     end = 10;
+    for (int i = 10; i > 1; i--){
+      total -= i;
+    System.out.println(i);
+  }
+  }
+}
